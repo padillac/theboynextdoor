291855843d364c55c0b3b862b18263f990bd656c
padillac <shinimaninima@gmail.com>
2018-12-31 21:31:51 -0500

    more work on image script4

--- static/main-image-swap.js
+++ static/main-image-swap.js
@@ -3,12 +3,12 @@ var bannerImg1 = document.getElementById('banner-image1');
 var bannerImg2 = document.getElementById('banner-image2');
 
 setInterval(function() {
-  bannerImg1.style = 'visibility: hidden;';
-  bannerImg2.style = 'visibility: visible;';
+  bannerImg1.animate({opacity: 0});
+  bannerImg2.animate({opacity: 1});
 }, 6000);
 setTimeout(function() {
   setInterval(function() {
-    bannerImg1.style = 'visibility: visible;';
-    bannerImg2.style = 'visibility: hidden;';
+    bannerImg1.animate({opacity: 1});
+    bannerImg2.animate({opacity: 0});
   }, 6000);
 }, 3000);
