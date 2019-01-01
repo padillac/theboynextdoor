fea4aaa0836effab6008bfccb2365b2877291468
padillac <shinimaninima@gmail.com>
2018-12-31 20:55:24 -0500

    working on javascript change image script2

--- static/main-image-swap.js
+++ static/main-image-swap.js
@@ -8,8 +8,9 @@ var mainImg = document.getElementById('banner-image');
     alert('HELLO');
   }, 5000);
 
-  setTimeout(function() {
-    mainImg.src = "files/photos/andreasontractor.png";
-    mainImg.alt = "Andreas on his tractor";
-    alert('HELLO2');
-  }, 5000);
+
+//  setTimeout(function() {
+//    mainImg.src = "files/photos/andreasontractor.png";
+//    mainImg.alt = "Andreas on his tractor";
+//    alert('HELLO2');
+//  }, 5000);
