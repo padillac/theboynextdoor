ac46537039a20221ae958e56485af6f779ff866a
padillac <shinimaninima@gmail.com>
2018-12-31 22:14:23 -0500

    working on fixing scroll buttons

--- static/nav-buttons.js
+++ static/nav-buttons.js
@@ -1,3 +1,19 @@
+var descriptionbutton = document.getElementById('descriptionbutton');
+var contactbutton = document.getElementById('contactbutton');
 var backtotopbutton = document.getElementById('backtotopbutton');
+
+
+descriptionbutton.onclick = function () {
+  window.scroll(0,1000);
+}
+contactbutton.onclick = function () {
+  window.scroll(0,1500);
+}
+backtotopbutton.onclick = function () {
+  window.scroll(0,2000);
+}
+
+
+
 
 // make back to top button appear when page is not fully scrolled up.
