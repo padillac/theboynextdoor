636440e0c0eff92882279327e7bf36be11d84789
padillac <shinimaninima@gmail.com>
2018-12-31 21:38:38 -0500

    more work on image script5

--- static/main-image-swap.js
+++ static/main-image-swap.js
@@ -3,12 +3,12 @@ var bannerImg1 = document.getElementById('banner-image1');
 var bannerImg2 = document.getElementById('banner-image2');
 
 setInterval(function() {
-  bannerImg1.animate({opacity: 0});
-  bannerImg2.animate({opacity: 1});
+  bannerImg1.animate([{opacity: 0}], {duration: 1000});
+  bannerImg2.animate([{opacity: 1}], {duration: 1000});
 }, 6000);
 setTimeout(function() {
   setInterval(function() {
-    bannerImg1.animate({opacity: 1});
-    bannerImg2.animate({opacity: 0});
+    bannerImg1.animate([{opacity: 1}], {duration: 1000});
+    bannerImg2.animate([{opacity: 0}], {duration: 1000});
   }, 6000);
 }, 3000);
