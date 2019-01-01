93306fb492bbacf607bf56deacdaf13eb812a220
padillac <shinimaninima@gmail.com>
2018-12-31 21:15:48 -0500

    more work on image script

--- static/main-image-swap.js
+++ static/main-image-swap.js
@@ -2,9 +2,6 @@
 var bannerImg1 = document.getElementById('banner-image1');
 var bannerImg2 = document.getElementById('banner-image2');
 
-
-  while (true) {
-    setTimeout(function () {
-      alert('HELLO2');
-    }, 5000,);
-  }
+setInterval(function() {
+  alert('hello');
+}, 3000);
