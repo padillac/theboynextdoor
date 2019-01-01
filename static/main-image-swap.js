d806a6d36bc08b01e78e12b3d8eadc7a304a4cd4
padillac <shinimaninima@gmail.com>
2018-12-31 22:02:19 -0500

    finished image transition script

--- static/main-image-swap.js
+++ static/main-image-swap.js
@@ -4,11 +4,15 @@ var bannerImg2 = document.getElementById('banner-image2');
 
 setInterval(function() {
   bannerImg1.animate([{opacity: 1}, {opacity: 0}], {duration: 1000});
+  bannerImg1.setAttribute('style', 'opacity: 0;');
   bannerImg2.animate([{opacity: 0}, {opacity: 1}], {duration: 1000});
+  bannerImg2.setAttribute('style', 'opacity: 1;');
 }, 6000);
 setTimeout(function() {
   setInterval(function() {
     bannerImg1.animate([{opacity: 0}, {opacity: 1}], {duration: 1000});
+    bannerImg1.setAttribute('style', 'opacity: 1;');
     bannerImg2.animate([{opacity: 1}, {opacity: 0}], {duration: 1000});
+    bannerImg2.setAttribute('style', 'opacity: 0;');
   }, 6000);
 }, 3000);
