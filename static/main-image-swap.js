167ece57d63542e7fed497be9a7d5ef2a7133772
padillac <shinimaninima@gmail.com>
2018-12-31 22:08:34 -0500

    finalized image moving styles

--- static/main-image-swap.js
+++ static/main-image-swap.js
@@ -7,12 +7,12 @@ setInterval(function() {
   bannerImg1.setAttribute('style', 'opacity: 0;');
   bannerImg2.animate([{opacity: 0}, {opacity: 1}], {duration: 1000});
   bannerImg2.setAttribute('style', 'opacity: 1;');
-}, 6000);
+}, 10000);
 setTimeout(function() {
   setInterval(function() {
     bannerImg1.animate([{opacity: 0}, {opacity: 1}], {duration: 1000});
     bannerImg1.setAttribute('style', 'opacity: 1;');
     bannerImg2.animate([{opacity: 1}, {opacity: 0}], {duration: 1000});
     bannerImg2.setAttribute('style', 'opacity: 0;');
-  }, 6000);
-}, 3000);
+  }, 10000);
+}, 5000);
